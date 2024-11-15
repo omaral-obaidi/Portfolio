64efebe4fe2105e994815b642a855a67b8c78156
Omar Al-Obaidi <omar1_alobaidi@yahoo.com>
2024-11-15 15:18:13 -0500

    time to fix it

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -3,8 +3,6 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
-import { Nav } from "react-bootstrap"; // Import Nav for Nav.Link
-import { Link } from "react-router-dom"; // Import Link for routing
 
 function ProjectCards(props) {
   return (
@@ -15,13 +13,6 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        {more && (
-          <Nav.Item>
-            <Nav.Link as={Link} to={more} className="btn btn-primary">
-              More
-            </Nav.Link>
-          </Nav.Item>
-        )}
         <Button variant="primary" href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "More"}
